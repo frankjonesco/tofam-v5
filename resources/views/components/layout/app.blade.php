fe496331173b5c105dd37eef760b2959c82cf80e
“Frank <frank@frankjones.co>
2024-01-16 21:48:42 +0000

    Install Butschster MetaDataTags and connected local database.

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
 
+    @meta_tags
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
